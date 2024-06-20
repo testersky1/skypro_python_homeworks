5e9dd3c7556db263b3acc2129c5cb60d6b9d9a3b
testersky1 <deadtriple666@gmail.com>
2024-06-20 19:26:51 +0500

    FirstHomWor

---
+++ Lesson1/lesson_1_task_6.py
@@ -0,0 +1,41 @@
+def funcA():
+	print(1 , end = "")
+
+def funcB():
+	print(2 , end = "")
+
+def funcC():
+	print(3 , end = "")
+
+def funcD():
+	print(4 , end = "")
+	
+def funcE():
+	print(5 , end = "")
+	
+def funcF():
+	print(6 , end = "")
+	
+def funcG():
+	print(7 , end = "")
+	
+def funcH():
+	print(8 , end = "")
+
+def funcJ():
+	print(9 , end = "")
+	
+def funcK():
+	print(0 , end = "")
+
+funcH()
+funcH()
+funcK()
+funcK()
+funcE()
+funcE()
+funcE()
+funcC()
+funcE()
+funcC()
+funcE()
