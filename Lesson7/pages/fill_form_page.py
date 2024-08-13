9f6860501abaaf327a5e0fa72b0c0161ce19d9a3
testersky1 <deadtriple666@gmail.com>
2024-08-13 19:43:03 +0500

    HW7_updatable_update

--- Lesson7/pages/fill_form_page.py
+++ Lesson7/pages/fill_form_page.py
@@ -1,6 +1,6 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+
 class FillFormPage:
 
     def __init__(self, driver):
@@ -9,6 +9,7 @@ class FillFormPage:
         self._driver.get('https://bonigarcia.dev/selenium-webdriver-java/data-types.html')
         self._driver.implicitly_wait(4)
 
+    
     def first_name(self, name):
         self._driver.find_element(By.NAME, 'first-name').click()
         self._driver.find_element(By.NAME, 'first-name').send_keys(name)
@@ -52,15 +53,37 @@ class FillFormPage:
     def submit(self):
         self._driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
         
+    def success_first_name(self):
+        first = self._driver.find_element(By.ID, 'first-name')
+        return first.value_of_css_property('background-color')
+    def success_last_name(self):
+        last = self._driver.find_element(By.ID, 'last-name')
+        return last.value_of_css_property('background-color')
+    def success_address(self):
+        address = self._driver.find_element(By.ID, 'address')
+        return address.value_of_css_property('background-color')
+    def success_e_mail(self):
+        e_mail_id = self._driver.find_element(By.ID, 'e-mail')
+        return e_mail_id.value_of_css_property('background-color')
+    def success_phone(self):
+        phone = self._driver.find_element(By.ID, 'phone')
+        return phone.value_of_css_property('background-color')
+    def success_city(self):
+        city = self._driver.find_element(By.ID, 'city')
+        return city.value_of_css_property('background-color')
+    def success_country(self):
+        country = self._driver.find_element(By.ID, 'country')
+        return country.value_of_css_property('background-color')
+    def success_job_position(self):
+        job_position = self._driver.find_element(By.ID, 'job-position')
+        return job_position.value_of_css_property('background-color')
+    def success_company(self):
+        company = self._driver.find_element(By.ID, 'company')
+        return company.value_of_css_property('background-color')
+    
+
     def empty_zip_field(self):
         zip = self._driver.find_element(By.ID, 'zip-code')
         return zip.value_of_css_property('background-color')
     
-    def get_element_by_class(self, class_name):
-        element = WebDriverWait(self._driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, class_name))
-        )
-        return element
     
-    def quit(self):
-        self._driver.quit()
--- Lesson7/test_lesson_7_task_1.py
+++ Lesson7/test_lesson_7_task_1.py
@@ -3,19 +3,28 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.color import Color
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 from pages.fill_form_page import FillFormPage
 
-
-def test_fill_form():
-
+@pytest.fixture(scope="function")  
+def driver():
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    yield driver 
+    driver.quit()  
+
+def test_fill_form(driver):
+
     test_fill_form = FillFormPage(driver)
     
     test_fill_form.first_name('Данил')
     test_fill_form.last_name('Тарханов')
     test_fill_form.address_field('Ленина, 55-3')
     test_fill_form.email_field('test@skypro.com')
+    #test_fill_form.email_field('')
     test_fill_form.phone_field('+7985899998787')
     test_fill_form.zip_code('')
     test_fill_form.city_field('Москва')
@@ -24,20 +33,14 @@ def test_fill_form():
     test_fill_form.company_field('SkyPro')
     test_fill_form.submit()
     
-    red = '#842029'  # '#FF0000'
-    green = '#0f5132'  # '#008000'
-
-    check_color_by_class(test_fill_form, "alert-danger", red)
-    check_color_by_class(test_fill_form, "alert-success", green)
-
-
-def check_color_by_class(page, class_name, expected_color):
-    field = page.get_element_by_class(class_name)
-    actual_color = Color.from_string(field.value_of_css_property('color')).hex
-    print(f"Expected color {expected_color}, but got {actual_color}")
-    assert actual_color == expected_color
-
-def quit():
-    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-    driver = FillFormPage(driver)
-    quit.quit()
+    assert test_fill_form.success_first_name() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_last_name() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_address() == 'rgb(209, 231, 221)'
+    assert test_fill_form.empty_zip_field() == 'rgb(248, 215, 218)'
+    assert test_fill_form.success_e_mail() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_phone() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_city() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_country() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_job_position() == 'rgb(209, 231, 221)'
+    assert test_fill_form.success_company() == 'rgb(209, 231, 221)'
+    
